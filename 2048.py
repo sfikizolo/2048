4643ae00678736118f6bd4978faa936329c74cac
@sfikizol021 <sfikizol021@student.wethinkcode.co.za>
2023-03-22 10:34:00 +0200

    drawing the border

--- 2048.py
+++ 2048.py
@@ -11,9 +11,34 @@ timer = pygame.time.Clock()
 fps = 60 
 font = pygame.font.Font('freesansbold.ttf', 24)
 
+colors = {0: (204, 192, 179),
+          2: (238, 228, 218),
+          4: (237, 224, 200),
+          8: (242, 177, 121),
+          16: (245, 149, 99),
+          32: (246, 124, 95),
+          64: (246, 94, 59),
+          128: (237, 207, 114),
+          256: (237, 204, 97),
+          512: (237, 200, 80),
+          1024: (237, 197, 63),
+          2048: (237, 194, 46),
+          'light text': (249, 246, 242),
+          'dark text': (119, 110, 101),
+          'other': (0, 0, 0),
+          'bg': (187, 173, 160)}
+
+def board():
+    pygame.draw.rect(screen, 'gray',[0,0,600,600],0,10)
+
+def pieces():
+    print('tiles')
+
 while True :
     timer.tick(fps)
     screen.fill('blue')
+    board()
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
