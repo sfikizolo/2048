78c6b860da2f602d658cab4b035df23028f3e3b5
@sfikizol021 <sfikizol021@student.wethinkcode.co.za>
2023-03-22 11:56:02 +0200

    moving the code around

--- 2048.py
+++ 2048.py
@@ -1,5 +1,6 @@
 import pygame 
 import random
+from GeneratePieces.Mypieces import pieces
 
 pygame.init()
 
@@ -11,7 +12,41 @@ timer = pygame.time.Clock()
 fps = 60 
 font = pygame.font.Font('freesansbold.ttf', 24)
 
-board_values = [[64 for _ in range(4) ] for _ in range(4)]
+def new_piece(board):
+    over = True
+    count = 0
+    while any(0 in row for row in board) and count <1:
+        row = random.randint(0,3)
+        col = random.randint(0, 3)
+        if board[row][col]==0:
+            count += 1
+            if random.randint(1, 10) == 10:
+                board[row][col] = 4
+            else:
+                board[row][col] = 2
+    
+    if count < 1 :
+        over = True    
+        
+    return board,over
+
+board_values = [[0 for _ in range(4) ] for _ in range(4)]
+game_over= False
+spawn_new = True
+start_count = 0
+
+
+def take_turn(direc,board):
+    merged = [[]]
+    if direc == 'UP':
+        pass
+    elif direc == 'DOWN':
+        pass
+    elif direc == 'LEFT':
+        pass
+    elif direc == 'RIGHT':
+        pass
+    return board
 
 colors = {0: (204, 192, 179),
           2: (238, 228, 218),
@@ -33,25 +68,25 @@ colors = {0: (204, 192, 179),
 def board():
     pygame.draw.rect(screen, 'gray',[0,0,600,600],0,10)
 
-def pieces(board):
-    for i in range(4):
-        for j in range(4):
-            value = board[i][j]
-            if value > 8:
-                value_color = colors['light text']
-            else:
-                value_color = colors['dark text']
-            if value <= 2048:
-                color =colors[value]
-            else:
-                color = colors['other']
-            pygame.draw.rect(screen,color , [j * 145 + 20, i * 145  +20,125,125],0,5)
-            if value > 0 :
-                value_len = len(str(value))
-                font = pygame.font.Font('freesansbold.ttf', 58 - (6*value_len))
-                value_text = font.render(str(value), True, value_color)
-                text_rect = value_text.get_rect(center=(j * 145 + 75,i * 145 + 75))
-                screen.blit(value_text,text_rect)
+# def pieces(board):
+#     for i in range(4):
+#         for j in range(4):
+#             value = board[i][j]
+#             if value > 8:
+#                 value_color = colors['light text']
+#             else:
+#                 value_color = colors['dark text']
+#             if value <= 2048:
+#                 color =colors[value]
+#             else:
+#                 color = colors['other']
+#             pygame.draw.rect(screen,color , [j * 145 + 20, i * 145  +20,125,125],0,5)
+#             if value > 0 :
+#                 value_len = len(str(value))
+#                 font = pygame.font.Font('freesansbold.ttf', 58 - (6*value_len))
+#                 value_text = font.render(str(value), True, value_color)
+#                 text_rect = value_text.get_rect(center=(j * 145 + 75,i * 145 + 75))
+#                 screen.blit(value_text,text_rect)
               
 
 
@@ -60,11 +95,30 @@ while True :
     screen.fill('blue')
     board()
     pieces(board_values)
+    direction = ''
+    if spawn_new or start_count < 2:
+        board_values , game_over = new_piece(board_values)
+        spawn_new = False
+        start_count +=1
+    if direction != '':
+        board_values = take_turn(direction,board_values)
+        direction = ''
+        spawn_new = True
 
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             break
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                direction = "UP"
+            elif event.key == pygame.K_DOWN:
+                direction = "DOWN" 
+            elif event.key == pygame.K_LEFT:
+                direction = "LEFT" 
+            elif event.key == pygame.K_RIGHT:
+                direction = "RIGHT" 
+           
 
     pygame.display.flip()
 pygame.quit()
